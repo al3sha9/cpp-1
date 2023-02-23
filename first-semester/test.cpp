95cfeb4f9dd94dc49a0be8cee1b3918d60eba034
al3sha9 <optimist.alishan@gmail.com>
2023-02-23 09:58:55 +0500

    LAB TASK 2

---
+++ first-semester/test.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+	int a = 8, b = 4;
+	int sub, mul, div, add;
+	
+	cout<<"The sum is = "<<add<<endl;
+	cout<<"The subtraction is = "<<sub<<endl;
+	cout<<"The multiplication is = "<<mul<<endl;
+	cout<<"The division is = "<<div;
+
+
+
+	return 0;
+
+	
+
+
+	// //First question
+	// cout<<"*\n";
+	// cout<<"* *\n";
+	// cout<<"* * *\n";
+	// cout<<"* * * *";
+	
+	//second question
+
+	// cout<<"*"<<setw(6)<<"*"<<endl;
+	// cout<<setw(2)<<"*"<<setw(4)<<"*"<<endl;
+	// cout<<setw(4)<<"*"<<endl;
+	// cout<<setw(2)<<"*"<<setw(4)<<"*"<<endl;
+	// cout<<"*"<<setw(6)<<"*"<<endl;
+	
+
+  //third question
+
+//   cout<<"********"<<endl;
+//   cout<<"********"<<endl;
+//   cout<<"********"<<endl;
+//   cout<<"********"<<endl;
+//   cout<<"********";
+
+
+}
