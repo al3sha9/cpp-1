3b4b5b03576054b6e015da55506e53793e96a75b
al3sha9 <optimist.alishan@gmail.com>
2023-03-09 20:01:18 +0500

    Space

--- quadratic.cpp
+++ quadratic.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
 
-float a, b, c, x1, x2, discriminant;
+float a, b, c, x1, x2, discriminant, realside, imaginary;
     cout << "Enter coefficients a, b and c: ";
     cin >> a >> b >> c;
     discriminant = b*b - 4*a*c;
@@ -11,8 +11,8 @@ float a, b, c, x1, x2, discriminant;
         x1 = (-b + sqrt(discriminant)) / (2*a);
         x2 = (-b - sqrt(discriminant)) / (2*a);
         
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
+        cout << "x1 = :" << x1 << endl;
+        cout << "x2 = :" << x2 << endl;
    
 
     return 0;
