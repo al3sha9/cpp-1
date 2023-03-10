2200cdff27ba200e51fbff36630d75c6d4c17fd1
al3sha9 <optimist.alishan@gmail.com>
2023-03-11 00:04:36 +0500

    explicit

---
+++ explicit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x { 10 };
+    int y { 4 };
+
+
+    double d { (double)x / y }; // convert x to a double so we get floating point division
+    std::cout << d; // prints 2.5
+
+    return 0;
+}
