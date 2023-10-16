7a9b02464f89fb11836123477c945f24a81301d0
Nami <fazilaroushanbeg@gmail.com>
2023-10-16 10:31:27 +0500

    pushing

--- first-mids/hishma.cpp
+++ first-mids/hishma.cpp
@@ -1,41 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int weather, humidity, wind;
-    cout<<"what is the Weather Today? \n 1.Sunny \n 2.Overcast \n 3.Rain"<< endl;
-    cin>>weather;
+    cout << "what is the Weather Today? \n 1.Sunny \n 2.Overcast \n 3.Rain" << endl;
+    cin >> weather;
 
     if (weather == 1)
     {
-        cout<<"\n You choose Sunny!";
-        cout<<"\n What is the humidity? \n 1. High \n 2.Normal"<<endl;
-        cin>>humidity;
+        cout << "\n You choose Sunny!";
+        cout << "\n What is the humidity? \n 1. High \n 2.Normal" << endl;
+        cin >> humidity;
         if (humidity == 1)
         {
-            cout<<"Today is a High Humidity Day"<<endl;
+            cout << "Today is a High Humidity Day" << endl;
         }
         else
         {
-            cout<<"Today is a low humidity Day"<<endl;
+            cout << "Today is a low humidity Day" << endl;
         }
     }
     else if (weather == 2)
     {
-        cout<<"Today is an Overcast Day"<<endl;
+        cout << "Today is an Overcast Day" << endl;
     }
     else if (weather == 3)
     {
-        cout<<"\n You choose Rain!";
-        cout<<"\n What is the Wind Like? \n 1. Strong \n 2.Weak"<<endl;
-        cin>>wind;
+        cout << "\n You choose Rain!";
+        cout << "\n What is the Wind Like? \n 1. Strong \n 2.Weak" << endl;
+        cin >> wind;
         if (wind == 1)
         {
-            cout<<"Today is a Windy Day"<<endl;
+            cout << "Today is a Windy Day" << endl;
         }
         else
         {
-            cout<<"Today is not a windy Day"<<endl;
+            cout << "Today is not a windy Day" << endl;
         }
     }
 
