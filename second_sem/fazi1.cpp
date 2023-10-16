f9046bfd738bd89523e5591e9a901de3c1bf45ff
Nami <fazilaroushanbeg@gmail.com>
2023-10-16 10:05:15 +0500

    one done

--- second_sem/fazi1.cpp
+++ second_sem/fazi1.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"HELLO WORLD"<<endl;
+class foo
+{
+private:
+    static int count;
+
+public:
+    foo()
+    {
+        count++;
+    }
+    int get() const
+    {
+        return count;
+    }
+};
+
+int foo::count = 0;
+
+int main()
+{
+    foo f1, f2, f3;
+    cout << "Count: " << f1.get() << endl;
+    cout << "Count: " << f2.get() << endl;
+    cout << "Count: " << f3.get() << endl;
     return 0;
 }
