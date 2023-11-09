b5b1ab67657bad4e98281b17a1c5e5a708ce85ad
Ali Shan <optimist.alishan@gmail.com>
2023-11-10 04:52:58 +0500

    push

--- second_sem/oop_basic.cpp
+++ second_sem/oop_basic.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-class Student
+class Distance
 {
 private:
-    string name;
-    int roll_no;
+    int feet;
+    float inches;
 
 public:
-    void studData(string stdName, int stdroll)
+    Distance()
     {
-        name = stdName;
-        roll_no = stdroll;
+        feet = 0;
+        inches = 0.00;
+    }
+    Distance(int ft, float ic)
+    {
+        feet = ft;
+        inches = ic;
+    }
+    void getdist()
+    {
+        cout << "\nEnter feet: ";
+        cin >> feet;
+        cout << "Enter inches: ";
+        cin >> inches;
     }
 
-    void displayStudentData() const
+    void showdist()
     {
-        cout << "Student Name: " << name << endl;
-        cout << "Roll Number: " << roll_no << endl;
+        cout << feet << "\'-" << inches << '\"';
+    }
+
+    void addD(Distance d2, Distance d3)
+    {
+        inches += d2.inches + d3.inches;
+        feet = 0;
+
+        if (inches >= 12.0)
+        {
+            inches -= 12.0;
+            feet++;
+        }
+        feet += d2.feet + d3.feet;
     }
 };
-
 int main()
 {
-    Student St1;
+    Distance d1, d3;
+    Distance d2(11, 6.25);
 
-    St1.studData("John", 2);
+    d1.getdist();
+    d3.addD(d1, d2);
 
-    St1.displayStudentData();
-
+    cout << "dist 1"; d1.showdist();
+    cout << "dist 2"; d2.showdist();
+    cout << "dist 3"; d3.showdist();
     return 0;
 }
