05cac30664fb6fba4996be0d5d564c7ab45586a4
Ali Shan <optimist.alishan@gmail.com>
2023-11-24 04:56:46 +0500

    pushing

--- 4.cpp
+++ 4.cpp
@@ -1,1 +1,54 @@
-4.cpp
+#include <iostream>
+using namespace std;
+
+class Vehicles
+{
+private:
+    float speed;
+    float fuel;
+public:
+    Vehicles(float s, float f){
+        speed = s;
+        fuel = f;
+    };
+    void disall(){
+        cout<<speed<<fuel;
+    }
+    ~Vehicles(){
+        cout<<"bye bye";
+    };
+};
+
+class Car : public Vehicles{
+    private:
+    string color;
+    public:
+    Car(float s, float f, string c) : Vehicles(s, f), color(c) {};
+    
+    void dis(){
+        cout<<"CAR DETAILS"<<endl;
+        disall();
+        cout<<color<<endl;
+    }
+};
+class Bike : public Vehicles{
+    private:
+    int eng;
+    public:
+    Bike(float s, float f, int c) : Vehicles(s, f), eng(c) {};
+    
+    void dis(){
+        cout<<"Bike DETAILS"<<endl;
+        disall();
+        cout<<eng<<endl;
+    }
+};
+
+int main(){
+    Car c1(240, 24, "black");
+    Bike b1(180, 3, 70);
+    c1.dis();
+    c1.dis();
+
+    return 0;
+}
