0cc83d68fb2a68c9496a9dbc43c5120951830616
Ali Shan <optimist.alishan@gmail.com>
2023-11-24 04:20:01 +0500

    pushing

---
+++ neww.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Animals
+{
+private:
+    int age;
+    string name;
+
+public:
+    Animals(int a, string n)
+    {
+        age = a;
+        name = n;
+    }
+    int getAge()
+    {
+        return age;
+    }
+    string getName()
+    {
+        return name;
+    }
+};
+
+class Mammals : public Animals
+{
+public:
+    Mammals(string n, int a) : Animals(a, n) {}
+};
+
+class Reptiles : public Animals
+{
+public:
+    Reptiles(string n, int a) : Animals(a, n) {}
+};
+
+class Birds : public Animals
+{
+public:
+    Birds(string n, int a) : Animals(a, n) {}
+};
+
+int main()
+{
+    Mammals lion("NIGGA", 12);
+    Reptiles liz("MONEY", 420);
+    Birds nig("FUCK", 69);
+
+    cout << "MAMMAL" << lion.getAge() << lion.getName() << endl;
+    cout << "Reptiles" << liz.getAge() << liz.getName() << endl;
+    cout << "Birds" << nig.getAge() << nig.getName() << endl;
+}
