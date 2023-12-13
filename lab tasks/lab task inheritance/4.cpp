0b19227408dc1a4603c646c932ec81cee79c3649
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2023-12-13 09:22:52 +0500

    push

--- lab tasks/lab task inheritance/4.cpp
+++ lab tasks/lab task inheritance/4.cpp
@@ -3,17 +3,39 @@
 using namespace std;
 
 class Person{
-
+    protected:
+     string name = "Fazila";
+     int age = 20;
+    public:
+     void displayDetails(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+     }
 };
 class Teaching{
-
+    protected:
+     string subject = "CS";
+     int experience = 4;
+    public:
+     void teach(){
+        cout<<"Teacher is teaching "<<subject<<" and have "<<experience<< " years experience."<<endl;
+     } 
 };
 class NonTeaching{
-
+    protected:
+     string position = " chef ", responsibility = "cooks for the staff";
+    public:
+     void performDuties(){
+        cout<<"He/she is"<<position<<" and "<<responsibility<<endl;
+     }
 };
-class StaffMember{
+class StaffMember : public Teaching, public NonTeaching{
 
 };
 int main(){
-    
+    Person personObject;
+    personObject.displayDetails();
+    StaffMember staffMemberObject;
+    staffMemberObject.teach();
+    staffMemberObject.performDuties();
 }
