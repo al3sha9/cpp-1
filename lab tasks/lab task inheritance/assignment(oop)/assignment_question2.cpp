f70cb40136fa39ed3877af7faba2b08f022119e0
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2023-12-14 20:18:20 +0500

    push

--- lab tasks/lab task inheritance/assignment(oop)/assignment_question2.cpp
+++ lab tasks/lab task inheritance/assignment(oop)/assignment_question2.cpp
@@ -2,11 +2,13 @@
 #include<cmath>
 using namespace std;
 
+
 class Number{
     protected:
         float a,b,c;
 
     public:
+        Number(){}
         Number(float a, float b, float c){
             this -> a = a;
             this -> b = b;
@@ -16,13 +18,27 @@ class Number{
         friend void display(Number);
 };
 class Computation{
-
+    public:
+    int squareOfSums(Number n){
+        return (n.a * n.a + n.b * n.b + n.c * n.c + 2 * n.a * n.b + 2 * n.b * n.c + 2 * n.c * n.a);
+    }
+    int squareOfDifference(Number n){
+        return(n.a * n.a - n.b * n.b - n.c * n.c - 2 * n.a * n.b - 2 * n.b * n.c - 2 * n.c * n.a);
+    }
+    int meanOfThreeNumber(Number n){
+        return (n.a * n.b * n.c)/3;
+    }
 };
-void display(Number){
-    cout<<a<<endl;
-    cout<<b<<endl;
-    cout<<c<<endl;
+void display(Number n){
+    cout<<"a is: "<<n.a<<endl;
+    cout<<"b is: "<<n.b<<endl;
+    cout<<"c is: "<<n.c<<endl;
 }
 int main(){
-
+    Number numberObject(2,3,4);
+    display(numberObject);
+    Computation computationObject;
+    cout<<"Sum: "<<computationObject.squareOfSums(numberObject)<<endl;
+    cout<<"Difference: "<<computationObject.squareOfDifference(numberObject)<<endl;
+    cout<<"Mean: "<<computationObject.meanOfThreeNumber(numberObject)<<endl;
 }
