cf6c08a7e16290c316c46ff349e4c8784446db62
Ali Shan <optimist.alishan@gmail.com>
2024-01-12 02:39:10 +0500

    pushging

--- 2-final-exam/delete.cpp
+++ 2-final-exam/delete.cpp
@@ -1,1 +1,34 @@
-delete.cpp
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base created\n";
+    }
+    virtual ~Base()
+    {
+        cout << "base destroyed\n";
+    }
+};
+
+class Der : public Base
+{
+public:
+    Der()
+    {
+        cout << "derived created\n";
+    }
+    virtual ~Der()
+    {
+        cout << "derived deleted\n";
+    }
+};
+
+int main()
+{
+    Base *b = new Der;
+    delete b;
+}
