256c91a229a7582a797b87d3f71fc8eb730807c2
Ali Shan <optimist.alishan@gmail.com>
2024-01-22 03:29:01 +0500

    pushing

--- lab tasks/lab/2.cpp
+++ lab tasks/lab/2.cpp
@@ -1,21 +1,26 @@
-//GCR question 1
-#include<iostream>
-#include<string>
+// GCR question 1
+#include <iostream>
+#include <string>
 using namespace std;
-class Person{
-  private:
-  //these attributes are private and hidden from external excess
+class Person
+{
+private:
+  // these attributes are private and hidden from external excess
   string name;
   int age;
   string address;
-  
-  Person(string name, string address, int age){
-      name = name;
-      address = address;
-      age = age;
+
+public:
+  Person() {}
+  Person(string name, string address, int age)
+  {
+    name = name;
+    address = address;
+    age = age;
   }
 };
 
-int main(){
-    Person p1;
+int main()
+{
+  Person p1;
 }
